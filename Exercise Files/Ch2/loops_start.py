33ad4e0833164f479e4027b9bce826299b5451c5
Eric Andrew Luna <ericluna1992@gmail.com>
2017-06-22 17:01:55 -0500

    updated

--- Exercise Files/Ch2/loops_start.py
+++ Exercise Files/Ch2/loops_start.py
@@ -4,6 +4,19 @@
 
 def main():
   x = 0
+
+  # define a while loop
+  #while (x < 5):
+   #  print (x)
+    # x = x + 1
   
+  #for x in range(1,10):
+  #	print (x)
+
+  days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+  for i,d in enumerate(days):
+    print (i,d)
+
+
 if __name__ == "__main__":
   main()
--- Exercise Files/Ch3/dates_start.py
+++ Exercise Files/Ch3/dates_start.py
@@ -2,8 +2,15 @@
 # Example file for working with date information
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 def main():
+	## DATE OBJECTS
+	# Get today's date from the simple today() method from the date class
+	today = date.today()
+	print ("Today's date is ", today)
   
   
 if __name__ == "__main__":
