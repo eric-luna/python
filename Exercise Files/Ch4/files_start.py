6e4e064d2d2ca9bd96a4db568e49b51acd398be7
Eric Andrew Luna <ericluna1992@gmail.com>
2017-07-27 11:13:42 -0500

    workign on chapter 5

--- Exercise Files/Ch4/files_start.py
+++ Exercise Files/Ch4/files_start.py
@@ -3,6 +3,17 @@
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
 def main():  
-    
+	# Open a file for writing and create it if it doesn't exist
+	# f = open("textfile.txt","w+")
+	
+	# append to file
+	f = open("textfile.txt","a+")
+
+	for i in range(10):
+		f.write("This is the line %d\r\n" % (i+1))
+
+	#close the file when done
+	f.close
+
 if __name__ == "__main__":
   main()
