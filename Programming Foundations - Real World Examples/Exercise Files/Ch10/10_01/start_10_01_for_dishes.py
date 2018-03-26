470cbcd4d17ddc277df009f6c146e9fdf18705ca
Eric Luna <ericluna1992@gmail.com>
2018-03-26 01:28:29 -0500

    deleted folders

--- Programming Foundations - Real World Examples/Exercise Files/Ch10/10_01/start_10_01_for_dishes.py
+++
@@ -1,9 +0,0 @@
-""" Loading the Dishwasher """
-
-# dirty dishes in the sink
-sink = ['bowl','plate','cup']
-
-for dish in list(sink):
-    print('Putting {} in the dishwasher'.format(dish))
-    sink.remove(dish)
-    print(sink)
